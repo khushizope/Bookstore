4f8046065e383eb905e89586494b726d82944389
khushizope <76126723+khushizope@users.noreply.github.com>
2022-04-03 17:21:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Bookstore
+# online-book-store-project-in-php
+
+more details and demo http://projectworlds.in/online-book-store-project-in-php/
---
+++ footer.php
@@ -0,0 +1,20 @@
+      	<hr>
+
+      	<footer>
+        	<!-- <div class="text-muted pull-left">
+            	<a href="http://projectworlds.in" target="_blank"> projectworlds </a>
+        	</div> -->
+        	<div class="text-muted pull-right" style="background-color:#64991e;padding:10px;border-radius:5px">
+          		<a href="admin.php" style="text-decoration:none;font-size:15px;font-weight:500;color:white ">Admin Login</a>
+        	</div>
+      	</footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
